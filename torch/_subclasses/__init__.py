9cefb9c71e53b6c1b1a041ed9fcc279c4515a6f7
Elias Ellison <eellison@devfair044.h1.fair>
2022-05-20 13:55:56 -0700

    Add FakeTensorMode
    ghstack-source-id: 62ea3677512a9b38d5d38a861cb71fcda6fcfdb3
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/77972

--- torch/_subclasses/__init__.py
+++ torch/_subclasses/__init__.py
@@ -1,10 +1,11 @@
 import torch
 
 from torch._subclasses.base_tensor import BaseTensor
-from torch._subclasses.fake_tensor import FakeTensor, _device_not_kwarg_ops
+from torch._subclasses.fake_tensor import FakeTensor, _device_not_kwarg_ops, _is_tensor_constructor
 
 _all__ = [
   "BaseTensor",
   "FakeTensor",
   "_device_not_kwarg_ops"
+  "_is_tensor_constructor"
 ]
